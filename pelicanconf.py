4cb9a598ebbcee881183b3b4ed5d4c23efc99611
Alexis Métaireau <alexis@notmyidea.org>
2014-07-24 02:44:18 +0200

    Yes, we have a blog

---
+++ pelicanconf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Alexis Métaireau'
+SITENAME = u'The Spiral Project'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/spiral-project'),)
+
+DEFAULT_PAGINATION = False
+
+THEME = "sundown"
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
